90948cc98e847cf2df45df4c14c5458b3a212d5b
JoseFredes <jose.fredes@mayor.cl>
2024-11-23 07:19:43 -0300

    Refactor history page layout and remove unnecessary code

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,24 +1,11 @@
 import { Tabs, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
-import { Menu } from "lucide-react";
 import Image from "next/image";
-import { Button } from "../components/button/Button";
 import { Card } from "../components/card/Card";
 import { CardContent } from "../components/card/CardContent";
 
 export default function PatientHistory() {
   return (
     <div className="flex justify-center bg-gray-50">
-      {/* <header className="border-b bg-white">
-        <div className="container flex h-14 items-center px-4">
-          <Button variant="ghost" size="icon" className="mr-3">
-            <Menu className="h-5 w-5" />
-          </Button>
-          <h1 className="text-lg font-semibold">
-            Historial de cuidados de Persona
-          </h1>
-        </div>
-      </header> */}
-
       <main className="container">
         <div className="space-y-4 pt-4">
           <PatientCard title="Azitromicina - 2cdas" timestamp="Hoy 9am" />
